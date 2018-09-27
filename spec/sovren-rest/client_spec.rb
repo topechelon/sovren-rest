55b64cfbc3d319ab21599a76f1e5bba1be0d5ca6
Michael Gruesen <mgruesen7@gmail.com>
2018-09-27 17:06:32 -0400

    Update tests; Add test for parse_html

--- spec/sovren-rest/client_spec.rb
+++ spec/sovren-rest/client_spec.rb
@@ -3,19 +3,24 @@ require 'base64'
 require 'rest-client'
 
 RSpec.describe SovrenRest::Client do
-  it 'should create new client' do
+  it 'should create #new' do
     expect(SovrenRest::Client).to receive(:new).with(
       {
-        :account_id=>"12345678",
-        :base_url=>"google.com",
-        :service_key=>"thiscanbeanything,whyrequireit?"
+        :accountId=>"12345678",
+        :baseUrl=>"google.com",
+        :serviceKey=>"thiscanbeanything,whyrequireit?"
       }
     )
-    SovrenRest::Client.new(base_url: 'google.com', account_id: '12345678', service_key: 'thiscanbeanything,whyrequireit?')
+    SovrenRest::Client.new(baseUrl: 'google.com', accountId: '12345678', serviceKey: 'thiscanbeanything,whyrequireit?')
   end
 
-  it 'should call parse and throw error' do
-    client = SovrenRest::Client.new(base_url: 'google.com', account_id: '12345678', service_key: 'thiscanbeanything,whyrequireit?')
+  it 'should call #parse and throw error' do
+    client = SovrenRest::Client.new(baseUrl: 'google.com', accountId: '12345678', serviceKey: 'thiscanbeanything,whyrequireit?')
+    expect{client.parse('rawFileContents')}.to raise_error RestClient::NotFound
+  end
+
+  it 'should call #parse_html and throw error' do
+    client = SovrenRest::Client.new(baseUrl: 'google.com', accountId: '12345678', serviceKey: 'thiscanbeanything,whyrequireit?')
     expect{client.parse('rawFileContents')}.to raise_error RestClient::NotFound
   end
 end
