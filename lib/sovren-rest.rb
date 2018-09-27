958911cb8b0eea2f0dbf1af8055f2af31c390cc4
Michael Gruesen <mgruesen7@gmail.com>
2018-09-27 16:40:31 -0400

    Clean up module definition

--- lib/sovren-rest.rb
+++ lib/sovren-rest.rb
@@ -1,17 +1,9 @@
-require 'rest-client'
-
 module SovrenRest
   class << self
-    FIELDS = [:endpoint, :username, :password, :timeout, :hard_time_out_multiplier,
-      :parser_configuration_params
-    ]
-    attr_accessor(*FIELDS)
-
     def configure
       yield self
       true
     end
-  end
-
   require_relative "client"
+  end
 end
