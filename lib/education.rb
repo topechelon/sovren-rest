5546a260c13d1a061529f2b7117cd249daef1d9b
Michael Gruesen <mgruesen7@gmail.com>
2018-10-02 16:53:44 -0400

    Add education class, spec

---
+++ lib/education.rb
@@ -0,0 +1,9 @@
+module SovrenRest
+  class Education
+    attr_reader :school_name, :degree_type, :major: :graduated, :end_date
+
+    def initialize(data={})
+
+    end
+  end
+end
---
+++ spec/sovren-rest/education_spec.rb
@@ -0,0 +1,70 @@
+require 'education.rb'
+
+RSpec.describe do
+  before :all do
+    @school_name = "University of Stuff"
+    @degree_type = "masters"
+    @major = "Stuff N Things"
+    @end_date = "2016-08"
+    @graduated = "false"
+
+    input =
+    {
+      "SchoolOrInstitution": [
+        {
+          "@schoolType": "university",
+          "School": [
+            {
+              "SchoolName": @school_name
+            }
+          ],
+          "PostalAddress": {
+            "CountryCode": "US",
+            "Region": [
+              "Ohio"
+            ],
+            "Municipality": "Akron"
+          },
+          "Degree": [
+            {
+              "@degreeType": @degree_type,
+              "DegreeName": "Master of Stuff in Things",
+              "DegreeMajor": [
+                {
+                  "Name": [
+                    @major
+                  ]
+                }
+              ],
+              "DatesOfAttendance": [
+                {
+                  "StartDate": {
+                    "AnyDate": "notKnown"
+                  },
+                  "EndDate": {
+                    "YearMonth": @end_date
+                  }
+                }
+              ],
+              "Comments": "University of Akron - Akron, Ohio\r\n\r\nMaster of Science in Computer Science, Aug 2016\r\nThesis: Towards an Ideal Execution Environment for Programmable Network Switches",
+              "UserArea": {
+                "sov:DegreeUserArea": {
+                  "sov:Id": "DEG-1",
+                  "sov:Graduated": @graduated,
+                  "sov:NormalizedDegreeName": "MSc",
+                  "sov:NormalizedDegreeType": "MSc"
+                }
+              }
+            }
+          ],
+          "UserArea": {
+            "sov:SchoolOrInstitutionTypeUserArea": {
+              "sov:NormalizedSchoolName": @school_name
+            }
+          }
+        }
+      ]
+    }
+
+  end
+end
