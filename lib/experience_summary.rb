c1ff6b3f3463b113f02f793edd8669b305370473
Michael Gruesen <mgruesen7@gmail.com>
2018-10-08 14:37:08 -0400

    Remove debugging

--- lib/experience_summary.rb
+++ lib/experience_summary.rb
@@ -4,7 +4,6 @@ module SovrenRest
     attr_reader :years_of_work_experience, :months_of_work_experience
 
     def initialize(data = {})
-      puts data
       @months_of_work_experience = data[:"sov:MonthsOfWorkExperience"].to_i
       @years_of_work_experience = (@months_of_work_experience / 12).floor
     end
