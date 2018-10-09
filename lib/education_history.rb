6434ff57b0d263875609b0c704218c93d36ceb88
Michael Gruesen <mgruesen7@gmail.com>
2018-10-09 10:19:37 -0400

    Rename

--- lib/education_history.rb
+++ lib/education_history.rb
@@ -1,10 +1,10 @@
 module SovrenRest
   # Represents an education record.
-  class Education
+  class EducationHistory
     attr_reader :school_name, :degree_type, :major, :graduated, :end_date
 
     def initialize(data = {})
-      school = data[:SchoolOrInstitution][0]
+      school = data['SchoolOrInstitution'][0]
 
       parse_school_name(school)
       parse_degree_type(school)
@@ -16,25 +16,25 @@ module SovrenRest
     private
 
     def parse_school_name(school = {})
-      @school_name = school[:School][0][:SchoolName]
+      @school_name = school['School'][0]['SchoolName']
     end
 
     def parse_degree_type(school = {})
-      @degree_type = school[:Degree][0][:@degreeType]
+      @degree_type = school['Degree'][0]['@degreeType']
     end
 
     def parse_major(school = {})
-      @major = school[:Degree][0][:DegreeMajor][0][:Name][0]
+      @major = school['Degree'][0]['DegreeMajor'][0]['Name'][0]
     end
 
     def parse_end_date(school = {})
-      @end_date = school[:Degree][0][:DatesOfAttendance][0][:EndDate]
+      @end_date = school['Degree'][0]['DatesOfAttendance'][0]['EndDate']
                   .map { |_k, v| v }[0]
     end
 
     def parse_graduated(school = {})
-      user_area = school[:Degree][0][:UserArea][:"sov:DegreeUserArea"]
-      @graduated = user_area[:"sov:Graudated"] ? 'true' : 'false'
+      user_area = school['Degree'][0]['UserArea']['sov:DegreeUserArea']
+      @graduated = user_area['sov:Graudated'] ? 'true' : 'false'
     end
   end
 end
