ef0c253458281bc51965e80269b5a57d3bc09e66
Michael Gruesen <mgruesen7@gmail.com>
2018-10-10 16:07:42 -0400

    Refactor

---
+++ lib/address.rb
@@ -0,0 +1,44 @@
+module SovrenRest
+  # Represents a contact information postal address.
+  class Address
+    attr_reader :line1, :line2, :city, :state,
+                :country, :postal_code
+
+    def initialize(data = {})
+      if data.nil? || !data.include?('PostalAddress')
+        @city = @state = @country = @postal_code = @line1 = @line2 = 'Unknown'
+      else
+        address = data.find { |cm| cm['PostalAddress'] }['PostalAddress']
+        parse_lines(address)
+        parse_city(address)
+        parse_state(address)
+        parse_country(address)
+        parse_postal_code(address)
+      end
+    end
+
+    private
+
+    def parse_lines(postal_address = {})
+      line = postal_address['DeliveryAddress']['AddressLine']
+      @line1 = line[0]
+      @line2 = line[1]
+    end
+
+    def parse_city(postal_address = {})
+      @city = postal_address['Municipality']
+    end
+
+    def parse_state(postal_address = {})
+      @state = postal_address['Region'][0]
+    end
+
+    def parse_country(postal_address = {})
+      @country = postal_address['CountryCode']
+    end
+
+    def parse_postal_code(postal_address = {})
+      @postal_code = postal_address['PostalCode']
+    end
+  end
+end
--- lib/contact_info.rb
+++ lib/contact_info.rb
@@ -1,9 +1,10 @@
+require 'address'
+
 module SovrenRest
   # Represents resume contact information.
   class ContactInfo
     attr_reader :first_name, :middle_name, :last_name, :email_addresses,
-                :websites, :phone_numbers, :address_line1, :address_line2,
-                :city, :state, :country, :postal_code
+                :websites, :phone_numbers, :address
 
     def initialize(data = {})
       parse_name(data['PersonName'])
@@ -11,7 +12,7 @@ module SovrenRest
       parse_email(contact_method)
       parse_websites(contact_method)
       parse_phone_numbers(contact_method)
-      parse_address(contact_method)
+      @address = SovrenRest::Address.new(contact_method)
     end
 
     # rubocop:disable Metrics/PerceivedComplexity, Metrics/MethodLength
@@ -33,42 +34,71 @@ module SovrenRest
     # rubocop:enable Metrics/PerceivedComplexity, Metrics/MethodLength
     # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity
 
+    def address_line1
+      @address.line1
+    end
+
+    def address_line2
+      @address.line2
+    end
+
+    def city
+      @address.city
+    end
+
+    def state
+      @address.state
+    end
+
+    def country
+      @address.country
+    end
+
+    def postal_code
+      @address.postal_code
+    end
+
     private
 
     def parse_name(person_name = {})
-      @first_name = person_name['GivenName']
-      @last_name = person_name['FamilyName']
-      @middle_name = person_name['MiddleName'] || ''
+      if person_name.nil?
+        @first_name = 'Unknown'
+        @last_name = 'Unknown'
+        @middle_name = 'Unknown'
+      else
+        @first_name = person_name['GivenName']
+        @last_name = person_name['FamilyName']
+        @middle_name = person_name['MiddleName'] || ''
+      end
     end
 
     def parse_email(contact_method = {})
-      @email_addresses = contact_method
-                         .map { |cm| cm['InternetEmailAddress'] }.compact
+      if contact_method.nil?
+        @email_addresses = []
+      else
+        @email_addresses = contact_method
+                           .map { |cm| cm['InternetEmailAddress'] }.compact
+      end
     end
 
     def parse_websites(contact_method = {})
-      @websites = contact_method
-                  .select { |cm| cm['InternetWebAddress'] }
-                  .map { |cm| cm['InternetWebAddress'] }
+      @websites = if contact_method.nil?
+                    []
+                  else
+                    contact_method
+                      .select { |cm| cm['InternetWebAddress'] }
+                      .map { |cm| cm['InternetWebAddress'] }
+                  end
     end
 
     def parse_phone_numbers(contact_method = {})
-      @phone_numbers = contact_method
-                       .select { |cm| cm['Telephone'] }
-                       .map { |phone| phone['Telephone']['FormattedNumber'] }
-    end
-
-    def parse_address(contact_method = {})
-      postal_address = contact_method
-                       .find { |cm| cm['PostalAddress'] }['PostalAddress']
-      address_line = postal_address['DeliveryAddress']['AddressLine']
-
-      @address_line1 = address_line[0]
-      @address_line2 = address_line[1]
-      @city = postal_address['Municipality']
-      @state = postal_address['Region'][0]
-      @country = postal_address['CountryCode']
-      @postal_code = postal_address['PostalCode']
+      if contact_method.nil?
+        @phone_numbers = []
+      else
+        @phone_numbers = contact_method
+                         .select { |cm| cm['Telephone'] }
+                         .map { |phone| phone['Telephone']['FormattedNumber'] }
+      end
     end
   end
 end
